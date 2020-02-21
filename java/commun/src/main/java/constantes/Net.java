08c69c5fe67145976748326ae01f070c944d8a77
Sofian <moumensofian@gmail.com>
2020-02-21 11:33:20 +0100

    #16 - Ajout d'une constante choix

--- java/commun/src/main/java/constantes/Net.java
+++ java/commun/src/main/java/constantes/Net.java
@@ -7,4 +7,5 @@ public class Net {
     public static final String AJOUTER = "add";
     public static final String VALEUR_CPT = "valeur";
     public static final String CONNEXION = "je me connecte";
+    public static final String CHOIX = "choix";
 }
