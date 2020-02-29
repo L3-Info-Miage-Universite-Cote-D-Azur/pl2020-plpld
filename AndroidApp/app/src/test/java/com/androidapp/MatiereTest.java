ab6a117f4f125a3bbf40992a80fa4b7f6f353ae8
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2020-02-29 18:37:52 +0100

    Modification de la classe MatiereTest #24

--- AndroidApp/app/src/test/java/com/androidapp/MatiereTest.java
+++ AndroidApp/app/src/test/java/com/androidapp/MatiereTest.java
@@ -2,20 +2,30 @@ package com.androidapp;
 
 import com.androidapp.reseau.Matiere;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MatiereTest {
 
-    private Matiere matiereDefault = new Matiere();
-    private Matiere matierePhysique = new Matiere("Physique");
-    private Matiere matiereMath = new Matiere("Mathématique");
-    private Matiere matiereInformatique = new Matiere("Informatique");
+    private Matiere matiereDefault;
+    private Matiere matierePhysique;
+    private Matiere matiereMath;
+    private Matiere matiereInformatique;
 
+    @Before
+    public void setup() {
+        //Initialisation
+        matiereDefault = new Matiere();
+        matierePhysique = new Matiere("Physique");
+        matiereMath = new Matiere("Mathématique");
+        matiereInformatique = new Matiere("Informatique");
+    }
 
     @Test
     public void nom() {
+        //Test avec le nom d'origine
         assertEquals("nom par défaut", matiereDefault.getNom().toString());
         assertEquals("Physique", matierePhysique.getNom().toString());
         assertEquals("Mathématique", matiereMath.getNom().toString());
@@ -26,6 +36,7 @@ public class MatiereTest {
         matiereMath.setNom("Math");
         matiereDefault.setNom("Electronique");
 
+        //Test avec un nom modifié grace a une méthode de la classe
         assertEquals("Electronique", matiereDefault.getNom().toString());
         assertEquals("Phys", matierePhysique.getNom().toString());
         assertEquals("Math", matiereMath.getNom().toString());
