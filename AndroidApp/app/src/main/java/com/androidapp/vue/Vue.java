e2da011c9c771b28cbee200fb1b9a3b576efe158
Sofian <moumensofian@gmail.com>
2020-03-02 18:21:03 +0100

    #39 - Création de la javadoc pour l'interface Vue

--- AndroidApp/app/src/main/java/com/androidapp/vue/Vue.java
+++ AndroidApp/app/src/main/java/com/androidapp/vue/Vue.java
@@ -5,8 +5,20 @@ import com.androidapp.reseau.*;
 
 import java.util.List;
 
+/**
+ *  Vue de l'écran
+ *
+ */
 public interface Vue {
+    /**
+     *  Affiche un message passé en paramètre à l'écran
+     * @param str Le message qui va être affiché
+     */
     void displayMsg(String str);
+
+    /**
+     *  Retourne la selection des UEs de l'utilisateur
+     */
     List<Matiere> selection();
 }
 
