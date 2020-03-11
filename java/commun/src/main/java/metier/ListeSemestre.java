347fe3bbd5e17ad34c1529873f02123e9e54d7bf
Sofian <moumensofian@gmail.com>
2020-03-11 18:30:34 +0100

    #54- Création d'une classe commune pour la gestion des UE

---
+++ java/commun/src/main/java/metier/ListeSemestre.java
@@ -0,0 +1,22 @@
+package metier;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ListeSemestre {
+
+    public HashMap<String, List<String>> getMapUE() {
+        return mapUE;
+    }
+
+
+    public void add(String key,List<String> value)
+    {
+        mapUE.put(key,value);
+    }
+    public void setMapUE(HashMap<String, List<String>> mapUE) {
+        this.mapUE = mapUE;
+    }
+
+    HashMap<String, List<String>> mapUE;
+}
