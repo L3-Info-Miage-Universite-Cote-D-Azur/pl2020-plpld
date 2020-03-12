4e761c1377271e6e0bec591dc029b6815bacf0a0
Sofian <moumensofian@gmail.com>
2020-03-12 20:38:01 +0100

    #54 - Modification de la méthode call() pour que le client puisse recevoir les différentes UE envoyées par le serveur

--- AndroidApp/app/src/main/java/com/androidapp/controleur/EcouteurDeReseau.java
+++ AndroidApp/app/src/main/java/com/androidapp/controleur/EcouteurDeReseau.java
@@ -4,11 +4,18 @@ import android.util.Log;
 
 import com.androidapp.vue.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.socket.emitter.Emitter;
 
 public class EcouteurDeReseau implements Emitter.Listener {
     private final Vue vue;
-
+    private List<Map<String,List<String>>> listOfMaps = new ArrayList<Map<String,List<String>>>();
     public EcouteurDeReseau(Vue vue) {
         this.vue = vue;
     }
@@ -16,6 +23,15 @@ public class EcouteurDeReseau implements Emitter.Listener {
     @Override
     public void call(Object... args) {
         Log.d("POUR MONTRER", "" + args[0]);
-        vue.displayMsg("Connexion serveur effectuée");
+        String[] tokens = args[0].toString().split("],");
+        HashMap<String,List<String>> map = new HashMap<>();
+        for( String str : tokens)
+        {
+            String[] matieres = str.substring(str.lastIndexOf("=[")).replace("=[","").split(",");
+            map.put(str.substring(0,str.indexOf("=[")), Arrays.asList(matieres));
+
+        }
+        listOfMaps.add(map);
+
     }
 }
