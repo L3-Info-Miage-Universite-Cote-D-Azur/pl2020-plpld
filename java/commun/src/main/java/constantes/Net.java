e1ccbafae869e82e70634d57133f06900db087fa
Sofian <moumensofian@gmail.com>
2020-03-13 19:32:09 +0100

    #60 - ajout des constantes pour les semestres

--- java/commun/src/main/java/constantes/Net.java
+++ java/commun/src/main/java/constantes/Net.java
@@ -10,4 +10,11 @@ public class Net {
     public static final String CHOIX = "choix";
     public static final String VALIDATION = "je valide mes choix";
     public static final String UE = "UE";
+    public static final String S1 = "commun/src/main/Ressource/S1.txt";
+    public static final String S2 = "commun/src/main/Ressource/S2.txt";
+    public static final String S3 = "commun/src/main/Ressource/S3.txt";
+
+    public static final String S4= "commun/src/main/Ressource/S4.txt";
+
+
 }
