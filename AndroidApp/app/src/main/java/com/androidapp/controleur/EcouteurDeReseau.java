67f562a227347791f43f48efdc289b53fdd016e2
Sofian <moumensofian@gmail.com>
2020-03-18 14:47:36 +0100

    #75 nettoyage du code et suppresion en simplifiant la conversion json->map

--- AndroidApp/app/src/main/java/com/androidapp/controleur/EcouteurDeReseau.java
+++ AndroidApp/app/src/main/java/com/androidapp/controleur/EcouteurDeReseau.java
@@ -2,9 +2,12 @@ package com.androidapp.controleur;
 
 import android.util.Log;
 
-import com.androidapp.vue.*;
 
-import java.lang.reflect.Array;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -15,17 +18,18 @@ import io.socket.emitter.Emitter;
 
 public class EcouteurDeReseau implements Emitter.Listener {
     public static List<Map<String,List<String>>> ListOfMaps = new ArrayList<Map<String,List<String>>>();
-
+    ObjectMapper objectMapper = new ObjectMapper();
+    HashMap<String,List<String>> map;
     @Override
     public void call(Object... args) {
         Log.d("POUR MONTRER", "" + args[0]);
-        String[] tokens = args[0].toString().split("],");
-        HashMap<String,List<String>> map = new HashMap<>();
-        for( String str : tokens)
-        {
-            String[] matieres = str.substring(str.lastIndexOf(":[")).replace(":[","").replace("\"" ,"").replace("]}","").split(",");
-            map.put(str.substring(0,str.indexOf(":[")).replace("\"","").replace("{",""), Arrays.asList(matieres));
+
+        try {
+            map = objectMapper.readValue(args[0].toString(), new TypeReference<Map<String,List<String>>>() {});
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+
         ListOfMaps.add(map);
     }
 }
