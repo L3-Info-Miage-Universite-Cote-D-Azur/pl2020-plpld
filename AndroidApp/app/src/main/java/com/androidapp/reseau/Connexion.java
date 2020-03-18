070af0b0d9f7012b0217d8438c6028889a707d00
Sofian <moumensofian@gmail.com>
2020-03-18 23:01:30 +0100

    #77 - Ajout des classes anonymes pour que le client puisse gérer plusieurs objets du serveur

--- AndroidApp/app/src/main/java/com/androidapp/reseau/Connexion.java
+++ AndroidApp/app/src/main/java/com/androidapp/reseau/Connexion.java
@@ -1,5 +1,12 @@
 package com.androidapp.reseau;
+import android.util.Log;
+
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import constantes.Net;
 import io.socket.client.IO;
@@ -9,25 +16,68 @@ import metier.ToJSON;
 
 import com.androidapp.controleur.*;
 import com.androidapp.vue.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Connexion {
     private Socket mSocket;
+
+    public List<Map<String, List<String>>> ListOfMaps = new ArrayList<Map<String,List<String>>>();
+
+    public Map<String,List<String>> MapPrerequis = new HashMap<>();
+
     public static String s;
     public void écouterRéseau() {
         try {
             mSocket = IO.socket("http://10.0.2.2:10101");
             EcouteurDeReseau net = new EcouteurDeReseau();
-            mSocket.on(Net.UE, net);
-
-            mSocket.on(Net.VALIDATION, new Emitter.Listener() {
+            mSocket.on(Net.UE, new Emitter.Listener() {
                 @Override
                 public void call(Object... args) {
-                    s=(String) args[0];
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    HashMap<String, List<String>> map = null;
+                    try {
+                        map = objectMapper.readValue(args[0].toString(), new TypeReference<Map<String, List<String>>>() {
+                        });
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    ListOfMaps.add(map);
                 }
             });
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+                    } catch (URISyntaxException ex) {
+            ex.printStackTrace();
         }
+
+       /* mSocket.on(Net.VALIDATION, new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            s = (String) args[0];
+                        }
+                    });*/
+
+        mSocket.on(Net.PREREQUIS, new Emitter.Listener() {
+            ObjectMapper objectMapper2 = new ObjectMapper();
+
+            @Override
+
+            public void call(Object... args) {
+
+                try {
+                    MapPrerequis = objectMapper2.readValue(args[0].toString(), new TypeReference<Map<String, List<String>>>() {
+                    });
+                    Log.d("TEST PREREQUIS",MapPrerequis.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+        /*} catch (URISyntaxException e) {
+
+            e.printStackTrace();
+        }*/
+            }
+        });
     }
 
     public void démarrerÉcoute() {
