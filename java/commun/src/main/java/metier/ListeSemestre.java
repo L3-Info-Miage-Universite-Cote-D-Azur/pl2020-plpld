2a9390f0edee8d4a6b1712e1f1b423c6ff7bcbdd
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2020-03-19 19:32:25 +0100

    Ajout javadoc pour la classe ListeSemestre #70

--- java/commun/src/main/java/metier/ListeSemestre.java
+++ java/commun/src/main/java/metier/ListeSemestre.java
@@ -1,21 +1,35 @@
 package metier;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Permet de faire un HashMap de la liste des matières par semestre
+ * Key (String) : Domaine de la matière (Informatiqque, Mathématiques, ...)
+ * Value (List<String>) : Liste des matières correspondant a son domaine (Bases de linfo, ...)
+ */
 public class ListeSemestre {
 
+    /**
+     * @return HashMap
+     */
     public HashMap<String, List<String>> getMapUE() {
         return mapUE;
     }
 
-
-    public void add(String key,List<String> value)
-    {
+    /**
+     * Ajoute un élément au HashMap
+     * @param key Domaine de la matière
+     * @param value Liste des matières correspondant a son domaine
+     */
+    public void add(String key,List<String> value) {
         mapUE.put(key,value);
     }
 
+    /**
+     * Permet de remplacer le HashMap
+     * @param mapUE Nouveau HashMap
+     */
     public void setMapUE(HashMap<String, List<String>> mapUE) {
         this.mapUE = mapUE;
     }
