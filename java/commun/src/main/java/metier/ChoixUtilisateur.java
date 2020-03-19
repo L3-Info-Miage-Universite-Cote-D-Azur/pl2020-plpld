cc0d51ecb8b7d52ff3c653b1952e2b2faa25edca
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2020-03-19 19:33:12 +0100

    Ajout javadoc pour la classe ChoixUtilisateur #70

--- java/commun/src/main/java/metier/ChoixUtilisateur.java
+++ java/commun/src/main/java/metier/ChoixUtilisateur.java
@@ -1,22 +1,33 @@
 package metier;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
+/**
+ *  Implémentation du choix de l'utilisateur.
+ *  Objet convertible en JSON pour que le serveur puisse recevoir l'information
+ */
 public class ChoixUtilisateur implements ToJSON {
     private List<Matiere> Choix;
     private int numSemestre;
 
+    /**
+     *  Constructeur prenant en paramètre une liste de matière.
+     *  Le champ Choix sera initialisé avec ce paramètre
+     * @param UserChoice  Liste de matière
+     */
     public ChoixUtilisateur(List<Matiere> UserChoice) {
         Choix = UserChoice;
     }
 
+    /**
+     * Constructeur prenant en paramètre un string
+     *  La chaine de caractère est analysée puis décomposée en UE afin de créer une nouvelle liste
+     */
     public ChoixUtilisateur(String S) {
         Choix = new ArrayList<Matiere>();
         S = S.replace("[", "");
@@ -27,18 +38,26 @@ public class ChoixUtilisateur implements ToJSON {
         numSemestre();
     }
 
+    /**
+     *  Retourne la liste des matières sélectionnées par l'utilisateur
+     * @return liste des matieres
+     */
     public List<Matiere> getChoix() {
         return Choix;
     }
 
+    /**
+     * Transforme la liste en objet JSON
+     * @return JSONObject de la liste
+     */
     @Override
     public JSONObject toJSON() {
-        JSONObject identité = new JSONObject();
+        JSONObject liste = new JSONObject();
         try {
-            identité.put("liste choisie", getChoix().toString());
+            liste.put("liste choisie", getChoix().toString());
         } catch (JSONException e) {
             e.printStackTrace();}
-        return identité;
+        return liste;
     }
 
     @Override
@@ -46,10 +65,16 @@ public class ChoixUtilisateur implements ToJSON {
         return Choix.toString();
     }
 
+    /**
+     * Permet d'obtenir le numero du semestre correspondant a la liste des matieres
+     */
     private void numSemestre() {
         numSemestre = Choix.get(0).getNom().charAt(1)-48;
         Choix.remove(0);
     }
 
+    /**
+     * @return le numero du semestre
+     */
     public int getNumSemestre() {return numSemestre;}
 }
