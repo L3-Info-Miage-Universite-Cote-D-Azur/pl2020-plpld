4a1e6adc74a435ef367c899334aa890abb3ae483
Sofian <moumensofian@gmail.com>
2020-03-25 16:52:43 +0100

    #91 - envoie de l'objet etudiant au serveur

--- AndroidApp/app/src/main/java/com/androidapp/vue/activity/InscriptionActivity.java
+++ AndroidApp/app/src/main/java/com/androidapp/vue/activity/InscriptionActivity.java
@@ -9,17 +9,25 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.androidapp.R;
+import com.androidapp.reseau.Connexion;
 
 import java.time.LocalDate;
 
+import constantes.Net;
 import metier.Etudiant;
+import metier.Identité;
+
+import static com.androidapp.vue.activity.HomeActivity.connexion;
 
 public class InscriptionActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        connexion.démarrerÉcoute();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
-
         final EditText nom = findViewById(R.id.nom);
         final EditText prénom = findViewById(R.id.prénom);
         final EditText numEtudiant = findViewById(R.id.numEtudiant);
@@ -28,14 +36,14 @@ public class InscriptionActivity extends AppCompatActivity {
 
         findViewById(R.id.buttonValiderInscription).setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
-                String[] naissance = dateNaissance.getText().toString().split("/");
-                if(naissance.length != 3) {
-                    Log.d("Erreur format", "Format de la date de naissance incorrect");
-                    //TODO 25-03-2020 : Création d'une exception en cas de saisie invalide
-                }
-                Etudiant etudiant = new Etudiant(nom.getText().toString(), prénom.getText().toString(), numEtudiant.getText().toString(), LocalDate.of(Integer.parseInt(naissance[2]), Integer.parseInt(naissance[1]), Integer.parseInt(naissance[0])), mdp.getText().toString());
-                startActivity(new Intent(InscriptionActivity.this, MainActivity.class));
+                   Etudiant etu = new Etudiant(nom.getText().toString(), prénom.getText().toString(),
+                            numEtudiant.getText().toString(), dateNaissance.getText().toString(), mdp.getText().toString());
+
+                    connexion.envoyerMessage(Net.NV_ETU,etu);
+
+                    startActivity(new Intent(InscriptionActivity.this, MainActivity.class));
                 }
         });
     }
