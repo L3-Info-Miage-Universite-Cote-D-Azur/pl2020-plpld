8fd5f247d4a569d6ed1af280f73f540082f7c969
Ossama <Ossama1998@windowslive.com>
2020-03-26 14:25:42 +0100

    Mise en place d'une barre de recherche des UE #73

--- java/commun/src/main/java/constantes/Net.java
+++ java/commun/src/main/java/constantes/Net.java
@@ -3,7 +3,6 @@ package constantes;
  * constante pour définir les messages entre le client et le serveur
  */
 public class Net {
-    
     public static final String CONNEXION = "je me connecte";
     public static final String CHOIX = "choix";
     public static final String VALIDATION = "je valide mes choix";
