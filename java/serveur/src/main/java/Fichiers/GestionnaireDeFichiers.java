31001da9841a044c69d9ded44379c0f21302412f
Sofian <moumensofian@gmail.com>
2020-03-26 18:34:01 +0100

    #90 - Ajout de méthodes pour gérer les enregistrements d'étudiants

--- java/serveur/src/main/java/Fichiers/GestionnaireDeFichiers.java
+++ java/serveur/src/main/java/Fichiers/GestionnaireDeFichiers.java
@@ -1,14 +1,14 @@
 package Fichiers;
 
+import constantes.Net;
 import metier.ListeSemestre;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static constantes.Net.BD;
 
 public class GestionnaireDeFichiers {
 
@@ -39,5 +39,51 @@ public class GestionnaireDeFichiers {
             e.printStackTrace();
         }
         return listeSemestre.getMapUE();
+    }
+
+    public void EcrireDansFichier(String fichier,String str) throws IOException {
+
+
+        try(FileWriter fw = new FileWriter(fichier, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(str);
+
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
+
+
+
+    }
+
+    public Boolean trouverEtudiant(String logs)
+    {
+        BufferedReader br;
+        try
+        {
+            br = new BufferedReader(new FileReader(BD));
+            String line = br.readLine();
+            while(line != null)
+            {
+                System.out.println(line);
+                if(line.equals(logs))
+                {
+                    br.close();
+                    return true;
+
+                }
+                line = br.readLine();
+            }
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
