6294cfd2f47b0b8dd7d14fbf391843ff36eabae7
SMoumen <moumensofian@gmail.com>
2020-04-27 11:06:10 +0200

    Mise à jour de la javadoc de GestionnaireDeReseau

--- java/serveur/src/main/java/reseau/GestionnaireDeReseau.java
+++ java/serveur/src/main/java/reseau/GestionnaireDeReseau.java
@@ -28,8 +28,17 @@ public class GestionnaireDeReseau {
      *  objet GestionnaireDeFichiers qui communique avec la classe
      */
     private GestionnaireDeFichiers FileHandler = new GestionnaireDeFichiers();
+
+    /**
+     *  Variable pour recupérer le numéro de l'étudiant et enregistrer son parcours avec un fichier texte à son nom
+     */
     private String tmpNum;
 
+    /**
+     *  Cette méthode enregistre le choix de parcours de l'étudiant dans un fichier texte correspondant à son numéro étudiant
+     * @param choix Le choix de parcours de l'étudiant
+     * @throws IOException
+     */
     public void nouvelleConfirmation(ChoixUtilisateur choix) throws IOException {
         System.out.println(choix.toString());
         FileHandler.EcrireDansFichierListe(tmpNum + ".txt",choix.getChoix());
@@ -37,8 +46,11 @@ public class GestionnaireDeReseau {
     }
 
     /**
-     *
-     * @param etudiant
+     *  Cette méthode enregistre l'étudiant passé en paramètre elle renvoie un boolean.
+     *  Si l'étudiant est déjà inscrit dans la base de donnée, cette méthode renvoie faux, ainsi le serveur
+     *  sait quoi faire, et transmettra cette information au client.
+     *  Sinon, l'étudiant est inscrit dans les bases de données.
+     * @param etudiant L'étudiant a enregistrer
      * @throws IOException
      */
     public boolean nouveauEtu(Etudiant etudiant) throws IOException {
@@ -56,6 +68,14 @@ public class GestionnaireDeReseau {
         }
     }
 
+    /**
+     *  Cette méthode renvoie un boolean. Elle vérifie si l'étudiant voulant se connecter existe bien dans la BD.
+     *  C'est à dire elle vérifie que les informations données par l'utilisateur sont valides.
+     *  Le résultat est renvoyé sous forme de boolean.
+     *  Le serveur communiquera cette information au client
+     * @param id L'identité de la personne voulant se connecter
+     * @return un boolean
+     */
     public boolean nouvelleConnexion(Identité id)
     {
 
@@ -63,7 +83,6 @@ public class GestionnaireDeReseau {
         if(FileHandler.trouverEtudiant(BD, id.getNom())) {
            System.out.println("accepté");
            return true;
-           //socketIOClient.sendEvent(NV_CONNEXION, "true");
         }
         else {
             System.out.println("refusé");
@@ -71,32 +90,61 @@ public class GestionnaireDeReseau {
         }
     }
 
-    /**
-     *   Valide le choix de l'étudiant et transmet la liste des UEs choisit par l'étudiant au client
-     * @param socketIOClient
-     * @param SelectionUE Liste de matières
-     */
-    public void validation(SocketIOClient socketIOClient, ChoixUtilisateur SelectionUE) {
-        if(SelectionUE != null)
-        {
-            socketIOClient.sendEvent(VALIDATION, SelectionUE.toString());
-          System.out.println("Votre sélection pour le semestre n°" + SelectionUE.getNumSemestre() + " (" + SelectionUE.toString() +") a été enregistrée.");
-    }}
 
+
+    /**
+     *  Les méthodes suivantes sont implémentées dans GestionnairesDeRéseau afin de faire le pont entre le Serveur et GestionnaireDeFichier
+     *  Une sorte d'entremetteur. Ainsi, elle retourne principalement les méthodes de GestionnaireDeFichiers.
+     */
+
+    /**
+     *  Cette méthode retourne la map du parcours prédefini.
+     *  Elle appelle la fonction de GestionnaireDeFichier et retourne la map construite
+     * @param path Le chemin du fichier à lire
+     * @return
+     */
     public Map<String, Map<Integer, List<String>>> lireFichierPredefini(String path) {
         return FileHandler.lireFichierPredefini(path);
     }
 
+    /**
+     *  Cette méthode retourne la map des prérequis
+     * @param s1 le chemin du S1
+     * @param s2 le chemin du S2
+     * @param s3 le chemin du S3
+     * @param s4 le chemin du S4
+     * @param fichierPrerequis le chemin du fichier des prerequis
+     * @return
+     */
     public HashMap<String, List<String>> lireConstructionPrerequis(String s1, String s2, String s3, String s4, String fichierPrerequis) {
         return FileHandler.constructionPrerequis(s1,s2,s3,s4,fichierPrerequis);
     }
 
+
+    /**
+     *
+     * @param path le fichier à lire
+     * @return la map construite à partir du fichier lu
+     */
     public HashMap<String, List<String>> lireFichier(String path) {
         return FileHandler.lireFichier(path);
     }
 
+    /**
+     *  Cette méthode retourne un étudiant grâce à son numéro étudiant, s'il existe dans la BD.
+     * @param numEtudiant
+     * @return un étudiant
+     */
     public Etudiant getEtudiant(String numEtudiant) {return FileHandler.getEtudiant("BD INFO ETUDIANTS.txt", numEtudiant); }
 
+    /**
+     *
+     * @param s1 le chemin du S1
+     * @param s2 le chemin du S2
+     * @param s3 le chemin du S3
+     * @param s4 le chemin du S4
+     * @return La liste de Map des fichiers lus
+     */
     public List<Map<String, List<String>>> lireTout(String s1, String s2, String s3, String s4) {
         return FileHandler.lireTout(s1,s2,s3,s4);
     }
