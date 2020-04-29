310511fe8b393654d4c064c330de502f6511ebed
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2020-04-29 18:34:49 +0200

    Ajout javadoc pour ServeurTest #122

--- java/serveur/src/test/java/serveur/ServeurTest.java
+++ java/serveur/src/test/java/serveur/ServeurTest.java
@@ -25,6 +25,9 @@ public class ServeurTest {
     }
 
 
+    /**
+     * On vérifie que la méthode "sendEvent" est bien effectué avec les bons paramètres dans la méthode "envoyerUETest".
+     */
     @Test
     public void envoyerUETest() {
         String semestreTest = "SemestreTest.txt";           // Fichier de test
@@ -33,19 +36,30 @@ public class ServeurTest {
     }
 
 
+    /**
+     * On vérifie que la méthode "sendEvent" est bien effectué avec les bons paramètres dans la méthode "envoyerTout".
+     */
     @Test
     public void envoyerToutTest() {
-        String semestreTest = "SemestreTest.txt";
+        String semestreTest = "SemestreTest.txt";           // Fichier de test
         serveur.envoyerTout(socketIOClient, semestreTest, semestreTest, semestreTest, semestreTest);
         Mockito.verify(socketIOClient).sendEvent(Mockito.eq(ENVOIE_TOUT), Mockito.anyList());
     }
 
+
+    /**
+     * On vérifie que la méthode "sendEvent" est bien effectué avec les bons paramètres dans la méthode "envoiePrerequis".
+     */
     @Test
     public void envoiePrerequisTest() {
         serveur.envoiePrerequis(socketIOClient);
         Mockito.verify(socketIOClient).sendEvent(Mockito.eq(PREREQUIS), Mockito.anyMap());
     }
 
+
+    /**
+     * On vérifie que la méthode "sendEvent" est bien effectué avec les bons paramètres dans la méthode "envoiePredefini".
+     */
     @Test
     public void envoiePredefiniTest() {
         String PredefiniTest = "PredefiniTest.txt";         // Fichier de test
@@ -53,6 +67,10 @@ public class ServeurTest {
         Mockito.verify(socketIOClient).sendEvent(Mockito.eq(PREDEFINI), Mockito.anyMap());
     }
 
+
+    /**
+     * On vérifie que la méthode "sendEvent" est bien effectué avec les bons paramètres dans la méthode "nouveauChoix".
+     */
     @Test
     public void nouveauChoixTest() {
         serveur.nouveauChoix(socketIOClient, "Math");
